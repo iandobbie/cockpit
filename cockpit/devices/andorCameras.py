ea9a73b3293e6cdd524afe4d929c74f22ba93f9a
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2018-07-30 14:35:40 +0100

    read isWaterCooled and targetTemperature from andorCamera config section

--- cockpit/devices/andorCameras.py
+++ cockpit/devices/andorCameras.py
@@ -105,8 +105,10 @@ class AndorCameraDevice(camera.CameraDevice):
         self.cached_settings={}
         self.settings['exposureTime'] = 0.001
         # Has water cooling? Default to False to ensure fan is active.
-        self.settings['isWaterCooled'] = False
-        self.settings['targetTemperature'] = -40
+        # but read from system config if we should enable water cooling. 
+        self.settings['isWaterCooled'] = self.config.get('isWaterCooled', False)
+        self.settings['targetTemperature'] = self.config.get(
+            'TargetTemperature', -40)
         self.settings['EMGain'] = 0
         self.settings['amplifierMode'] = None
         self.settings['triggerMode'] = 1
