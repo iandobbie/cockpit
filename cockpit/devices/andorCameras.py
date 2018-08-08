bdda70c47d20d11bbac9764316b35911598078ae
Ian Dobbie <ian.dobbie@bioch.ox.ac.uk>
2018-08-08 16:19:47 +0100

    fixed up setting water cooling and temp on remote

--- cockpit/devices/andorCameras.py
+++ cockpit/devices/andorCameras.py
@@ -106,9 +106,13 @@ class AndorCameraDevice(camera.CameraDevice):
         self.settings['exposureTime'] = 0.001
         # Has water cooling? Default to False to ensure fan is active.
         # but read from system config if we should enable water cooling. 
-        self.settings['isWaterCooled'] = self.config.get('isWaterCooled', False)
-        self.settings['targetTemperature'] = self.config.get(
-            'TargetTemperature', -40)
+        self.settings.update({'isWaterCooled':
+                              self.config.get('isWaterCooled', False)})
+        #set the water cooling on if config is True. 
+        if self.settings.get('isWaterCooled'):
+            self.proxy..update_settings({'isWaterCooled': True})
+        targetTemperature = 
+        self.setTargetTemperature(self.config.get('TargetTemperature', -40))
         self.settings['EMGain'] = 0
         self.settings['amplifierMode'] = None
         self.settings['triggerMode'] = 1
